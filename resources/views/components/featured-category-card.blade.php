bdf8c3fada3608cd21287be31eca6d566ffa90c7
Safwat Fathi <safwat.rashwan@gmail.com>
2025-10-28 16:49:08 +0300

    revert featured category card

--- resources/views/components/featured-category-card.blade.php
+++ resources/views/components/featured-category-card.blade.php
@@ -1,9 +1,8 @@
 <a href="{{ url('/category', $slug) }}">
 	<div class="card bg-base-200 shadow-xl cursor-pointer transition-all duration-300 hover:scale-105 hover:bg-base-300">
 			<div class="card-body text-center h-60 max-h-60">
-					<i data-lucide="{{ $icon }}" class="w-12 h-12 mx-auto mb-4 text-primary"></i>
-					<h3 class="card-title justify-center">{{ $title }}</h3>
-					<p>{{ $description }}</p>
+					<h3 class="card-title justify-center line-clamp-2">{{ $title }}</h3>
+					<p class="line-clamp-2">{{ $description }}</p>
 			</div>
 	</div>
 </a>

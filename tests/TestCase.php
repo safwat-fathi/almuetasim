49da505c3555ffc9ad84c8948ba776be87769a01
Safwat Fathi <safwat.rashwan@gmail.com>
2025-11-10 13:21:46 +0200

    fix: test step in CI

---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Create the application instance for testing.
+     */
+    public function createApplication(): Application
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
+
